43c2a6da2024f93f5657c71da18a736ce44a4d1f
zwe <zwezeya02@gmail.com>
2022-12-27 13:39:17 +0800

    change footer styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,9 @@ export default function Layout({ children }: PropsWithChildren) {
     return (
         <>
             <TopBar />
+            <body style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
             { children }
+            </body>
             <Footer />
         </>
     )
