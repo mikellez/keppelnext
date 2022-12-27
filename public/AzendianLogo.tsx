db2b56d9a450cbf932b5975eb2e0e2e585fea956
jon <20024680@myrp.edu.sg>
2022-12-27 15:12:20 +0800

    fixed svg complaints about shape rendering

--- public/AzendianLogo.tsx
+++ public/AzendianLogo.tsx
@@ -7,7 +7,7 @@ interface SVGInfo {
 export default function AzendianLogo(props: SVGInfo) {
 
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 7" width={props.size} height={props.size} shape-rendering="crispEdges">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 7" width={props.size} height={props.size} shapeRendering="crispEdges">
             <rect width="7" height="7" fill="#F15A23"/>
             <g fill="white">
                 <rect height="1" width="3" x="3" y="1"/>
