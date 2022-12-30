2d43ac05ecc80afd125b100e0a73a2cc0255fd3e
jon <20024680@myrp.edu.sg>
2022-12-30 09:53:17 +0800

    made top bar always at top of page

--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -8,7 +8,22 @@ export default function TopBar() {
 
 	return (
 		<div>
-			<div style={{backgroundColor: "#E3E3E3", height: "4rem", padding: "0.5rem", display: "flex", alignItems: "center"}}>
+			<div style={
+				{
+					position: "fixed",
+
+					top: 0,
+					height: "4rem",
+					width: "100%",
+					zIndex: "255",
+
+					padding: "0.5rem",
+
+					backgroundColor: "#E3E3E3",
+					display: "flex",
+					alignItems: "center"
+				}
+			}>
 				<NavBar />
 				<Image src="/keppellogo.png" alt="Keppell Logo" width={225} height={28}/>
 				<VscAccount size={28} style={{color:"#707070", marginLeft: "auto"}}/>
