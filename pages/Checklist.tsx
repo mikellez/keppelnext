650d729cd5101ecf80392a26dcae1e3697251943
jon <20024680@myrp.edu.sg>
2023-01-05 10:28:36 +0800

    checklist table added

--- pages/Checklist.tsx
+++ pages/Checklist.tsx
@@ -1,10 +1,97 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React, { useEffect, useState } from 'react'
 
-const Checklist = () => {
-  	return (
-		<div>Checklist</div>
-  	)
+import ModuleContent from '../components/ModuleContent'
+import ModuleHeader from '../components/ModuleHeader'
+import ModuleMain from '../components/ModuleMain'
+
+import { CompactTable } from '@table-library/react-table-library/compact';
+import { TableNode } from '@table-library/react-table-library/types/table';
+import { useTheme } from '@table-library/react-table-library/theme';
+import { getTheme } from '@table-library/react-table-library/baseline';
+
+import axios from 'axios';
+
+interface Checklist extends TableNode {
+	checklist_id: number;
+	created_date: Date;
+	chl_name: string;
+	description: string;
+	status_id: number;
+	createdbyuser: string;
+	assigneduser: string;
+	signoffuser: string;
+	plant_id: number
+	plant_name: string;
+	completeremarks_req: string;
+	linkedassets: string;
+	linkedassetids: string;
+	chl_type: string;
+	history: string;
+	nodes?: Checklist[] | null;
 }
 
-export default Checklist
+const COLUMNS: any[] = [
+	{ label: 'ID',					resize: true, renderCell: (item: Checklist) => item.checklist_id },
+	{ label: 'Details',				resize: true, renderCell: (item: Checklist) => item.chl_name },
+	{ label: 'Status',				resize: true, renderCell: (item: Checklist) => item.status_id },
+	{ label: 'Created On',			resize: true, renderCell: (item: Checklist) =>
+		item.created_date.toLocaleDateString('en-US', {
+		  year: 'numeric',
+		  month: '2-digit',
+		  day: '2-digit',
+		}),
+	},
+	{ label: 'Assigned To',			resize: true, renderCell: (item: Checklist) => item.assigneduser },
+	{ label: 'Signed Off By',		resize: true, renderCell: (item: Checklist) => item.signoffuser },
+	{ label: 'Created By',			resize: true, renderCell: (item: Checklist) => item.createdbyuser }
+];
+
+async function getChecklists() {
+	return await axios.get<Checklist[]>("http://localhost:3000/api/checklist/getTemplateChecklist/17")
+	.then((response) => {
+		response.data.forEach((s) => {
+			s.created_date = new Date(s.created_date)
+		});
+		return response.data;
+	})
+	.catch((e) => {
+		console.log("error getting checklists")
+		console.log(e);
+		return null;
+	});
+}
+
+export default function Checklist() {
+	const [checklistNodes, setChecklistNodes] = useState<Checklist[]>([]);
+	const theme = useTheme([
+		getTheme(),
+		{
+			Table: "--data-table-library_grid-template-columns:  5em calc(90% - 40em) 7em 8em 10em 10em 10%;",
+		},
+	]);
+	
+	useEffect(() => {
+		updateRequests()
+	}, []);
+
+	function updateRequests() {
+		getChecklists().then((c) => {
+			if(c == null)
+				return console.log("checklists null");
+
+			setChecklistNodes(c);
+		});
+	}
+	
+  	return (
+		<ModuleMain>
+			<ModuleHeader title="Checklist" header="Checklist">
+				<button className="btn btn-primary">New Checklist</button>
+				<button className="btn btn-primary">Export CSV</button>
+			</ModuleHeader>
+			<ModuleContent>
+				<CompactTable columns={COLUMNS} data={{nodes: checklistNodes}} theme={theme} layout={{custom: true}}/>
+			</ModuleContent>
+		</ModuleMain>
+  	)
+}
--- pages/Request.tsx
+++ pages/Request.tsx
@@ -12,7 +12,7 @@ import { getTheme } from '@table-library/react-table-library/baseline';
 
 import axios from 'axios';
 
-interface Requests extends TableNode {
+interface Request extends TableNode {
 	request_id: string;
 	created_date: Date;
 	fullname: string;
@@ -21,28 +21,28 @@ interface Requests extends TableNode {
 	plant_name: string;
 	priority: string;
 	status: string;
-	nodes?: Requests[] | null;
+	nodes?: Request[] | null;
 }
 
 const COLUMNS: any[] = [
-	{ label: 'ID',					resize: true, renderCell: (item: Requests) => item.request_id },
-	{ label: 'Fault Type',			resize: true, renderCell: (item: Requests) => item.fault_name },
-	{ label: 'Location',			resize: true, renderCell: (item: Requests) => item.plant_name },
-	{ label: 'Priority',			resize: true, renderCell: (item: Requests) => item.priority == null ? "-" : item.priority },
-	{ label: 'Status',				resize: true, renderCell: (item: Requests) => item.status },
-	{ label: 'Filter By Date',		resize: true, renderCell: (item: Requests) =>
+	{ label: 'ID',					resize: true, renderCell: (item: Request) => item.request_id },
+	{ label: 'Fault Type',			resize: true, renderCell: (item: Request) => item.fault_name },
+	{ label: 'Location',			resize: true, renderCell: (item: Request) => item.plant_name },
+	{ label: 'Priority',			resize: true, renderCell: (item: Request) => item.priority == null ? "-" : item.priority },
+	{ label: 'Status',				resize: true, renderCell: (item: Request) => item.status },
+	{ label: 'Filter By Date',		resize: true, renderCell: (item: Request) =>
 		item.created_date.toLocaleDateString('en-US', {
 		  year: 'numeric',
 		  month: '2-digit',
 		  day: '2-digit',
 		}),
 	},
-	{ label: 'Asset Name',			resize: true, renderCell: (item: Requests) => item.asset_name },
-	{ label: 'Requested By',		resize: true, renderCell: (item: Requests) => item.fullname }
+	{ label: 'Asset Name',			resize: true, renderCell: (item: Request) => item.asset_name },
+	{ label: 'Requested By',		resize: true, renderCell: (item: Request) => item.fullname }
 ];
 
 async function getRequests() {
-	return await axios.get<Requests[]>("/api/request/getRequests/")
+	return await axios.get<Request[]>("/api/request/getRequests/")
 	.then((response) => {
 		response.data.forEach((s) => {
 			s.created_date = new Date(s.created_date)
@@ -57,7 +57,7 @@ async function getRequests() {
 }
 
 export default function Request() {
-	const [requestNodes, setRequestNodes] = useState<Requests[]>([]);
+	const [requestNodes, setRequestNodes] = useState<Request[]>([]);
 	const theme = useTheme([
 		getTheme(),
 		{
