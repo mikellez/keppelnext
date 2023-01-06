1ea69c44d4ec62d0acc4d304478dc731ec06e72e
zwe <zwezeya02@gmail.com>
2023-01-06 18:00:55 +0800

    schedule props and interfaces

--- components/Schedule/ScheduleTemplate.tsx
+++ components/Schedule/ScheduleTemplate.tsx
@@ -1,15 +1,71 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useState, useEffect } from 'react';
 import { ModuleContent, ModuleHeader, ModuleMain } from '../';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
-interface ScheduleInfo extends PropsWithChildren {
+interface ScheduleTemplateInfo extends PropsWithChildren {
     title: string;
     header: string;
+    schedules?: ScheduleInfo[]; 
     timeline?: number;
-}
+};
 
-export default function ScheduleTemplate(props: ScheduleInfo) {
+export interface ScheduleInfo {
+    assigned_ids: number[]
+    calendar_dates: string[]
+    checklist_id: number;
+    checklist_name: string;
+    end_date: Date;
+    period: number;
+    plant: string;
+    remarks: string;
+    schedule_id: number;
+    start_date: Date;
+    username: string;
+};
+
+interface EventInfo {
+    title: string;
+    start: Date;
+    extendedProps: {
+        plant: string;
+        scheduleId: number;
+        startDate: Date;
+        endDate: Date;
+        recurringPeriod: string | number;
+        assignedTo: number[] | string[]
+        remarks: string
+    };
+};
+
+export default function ScheduleTemplate(props: ScheduleTemplateInfo) {
+    // Store the list of events in a state to be rendered on the calendar
+    const [eventList, setEventList] = useState<EventInfo[]>([]);
+
+    // Add events to be displayed on the calendar
+    useEffect(() => {
+        if (props.schedules) {
+            let newEvents:EventInfo[] = [];
+            props.schedules.forEach(item => {
+                const event = {
+                    title: item.checklist_name,
+                    start: new Date(item.start_date),
+                    extendedProps: {
+                        plant: item.plant,
+                        scheduleId: item.schedule_id,
+                        startDate: new Date(item.start_date),
+                        endDate: new Date(item.end_date),
+                        recurringPeriod: item.period,
+                        assignedTo: item.assigned_ids,
+                        remarks: item.remarks
+                    },
+                };
+                console.log(event)
+                newEvents.push(event);
+            });
+        }
+    }, []);
+
     return (
         <ModuleMain>
 			<ModuleHeader title={props.title} header={props.header}>
@@ -30,8 +86,10 @@ export default function ScheduleTemplate(props: ScheduleInfo) {
                         stickyHeaderDates={true}
                         selectable={true}
                         unselectAuto={true}
+                        events={eventList}
                 />
 			</ModuleContent>
 		</ModuleMain>
     )
 }
+
--- pages/Schedule/index.tsx
+++ pages/Schedule/index.tsx
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import ScheduleTemplate from '../../components/Schedule/ScheduleTemplate';
+import ScheduleTemplate, { ScheduleInfo } from '../../components/Schedule/ScheduleTemplate';
 import axios from 'axios';
 
 export default function Schedule() {
 	// Store the list of plants in a state for dropdown
-	const [plantList, setPlantList] = useState([]);
+	const [plantList, setPlantList] = useState<number[]>([]);
+	// Store the list of schedules in a state to be rendered on the calendar
+	const [scheduleList, setScheduleList] = useState<ScheduleInfo[]>([]);
 
 	// Calls an api on load to get the list of plants
 	useEffect(() => {
 		axios.get("/api/request/getScheduleAccess")
 		.then(res => {
-			console.log(res.data)
+			setScheduleList(res.data);
 		})
 		.catch(err => console.log(err.message))
 	}, []);
 	
   	return (
-		<ScheduleTemplate title="View Schedule" header="View Schedule">
+		<ScheduleTemplate title="View Schedule" header="View Schedule" schedules={scheduleList}>
 				<div className="form-group">
-					<select className="form-control">
+					{plantList && <select className="form-control">
 						{plantList.length > 1 && <option>View all Plants</option>}
-					</select>
+					</select>}
 				</div>
 		</ScheduleTemplate>
   	);
