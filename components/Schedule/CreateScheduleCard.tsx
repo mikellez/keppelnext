519d5fa93077908d955e1d29d753b0fb773829ff
zwe <zwezeya02@gmail.com>
2023-01-30 10:37:31 +0800

    styling for create schedule page

---
+++ components/Schedule/CreateScheduleCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "../../styles/Schedule.module.scss";
+
+export interface CreateScheduleCardProps {
+    title: string;
+    text: string;
+    icon: React.ReactNode; 
+    color: string;
+    path?: string; 
+};
+
+export default function CreateScheduleCard(props: CreateScheduleCardProps) {
+    return (
+        <div className={styles.createCard}>
+            <div> {props.icon} </div>
+            <h5> {props.title} </h5>
+            <p className={styles.createCardText}> {props.text} </p>
+            <button style={{backgroundColor: props.color}} className={styles.createCardBtn}> Select </button>
+            <span className={styles.createCardBar} style={{backgroundColor: props.color}}></span>
+        </div>
+    );
+};
--- pages/Schedule/Create/index.tsx
+++ pages/Schedule/Create/index.tsx
@@ -1,14 +1,51 @@
 import React from "react";
 import { ModuleContent, ModuleHeader, ModuleMain } from '../../../components';
+import CreateScheduleCard ,{ CreateScheduleCardProps } from "../../../components/Schedule/CreateScheduleCard";
+import { GiNewShoot } from 'react-icons/gi';
+import { RiDraftLine, RiCalendarCheckLine } from 'react-icons/ri';
+import styles from "../../../styles/Schedule.module.scss";
 
 export default function CreateSchedule() {
+    const createOptions: CreateScheduleCardProps[] = [
+        {
+            title: "New",
+            text: "Start afresh with a blank calendar",
+            icon: <GiNewShoot size={60} />,
+            color: "#CFFDE1"
+        },
+        {
+            title: "Approved",
+            text: "Work on the most recent approved schedule",
+            icon: <RiCalendarCheckLine size={60} />,
+            color: "#FFB200"
+        },
+        {
+            title: "Drafts",
+            text: "Continue from your collection of drafts",
+            icon: <RiDraftLine size={60} />,
+            color: "#91D8E4"
+        }
+    ];
+
+    const createOptionElements = createOptions.map(option => {
+        return <CreateScheduleCard
+            key={option.title}
+            title={option.title}
+            text={option.text}
+            icon={option.icon}
+            color={option.color}
+        />;
+    });
+
     return (
         <ModuleMain>
             <ModuleHeader title="Create Schedule" header="Create Schedule">
 
             </ModuleHeader>
             <ModuleContent>
-               
+                <div className={styles.createCardsContainer}>
+                    {createOptionElements}
+                </div>
             </ModuleContent>
         </ModuleMain>
     );
