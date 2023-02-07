929d18ba349bf4ff527168f330df48ee06fb881a
Xing Jie <tan.xing.jie@u.nus.edu>
2023-02-07 17:46:55 +0800

    scheduler manage information button

--- components/Schedule/TimelineSelect.tsx
+++ components/Schedule/TimelineSelect.tsx
@@ -1,39 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { CMMSTimeline } from '../../types/common/interfaces';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { CMMSTimeline } from "../../types/common/interfaces";
+import axios from "axios";
 
 interface TimelineSelectProps {
     onChange: React.ChangeEventHandler<HTMLSelectElement>;
     allStatus?: boolean;
     status: number;
     name: string;
-};
+}
 
 async function getTimelines(status: number, all: boolean = false) {
     const url = all ? "" : "/api/timeline/status/" + status;
-    return await axios.get<CMMSTimeline[]>(url)
-	.then(res => {
-		return res.data;
-	})
-	.catch(err => console.log(err.message));
-};
+    return await axios
+        .get<CMMSTimeline[]>(url)
+        .then((res) => {
+            return res.data;
+        })
+        .catch((err) => console.log(err.message));
+}
 
 export default function TimelineSelect(props: TimelineSelectProps) {
     // Store the list of timelines in a state for dropdown
-	const [timelineList, setTimelineList] = useState<CMMSTimeline[]>([]);
+    const [timelineList, setTimelineList] = useState<CMMSTimeline[]>([]);
 
     useEffect(() => {
-        getTimelines(props.status, props.allStatus).then(result => {
+        getTimelines(props.status, props.allStatus).then((result) => {
             if (result) {
                 setTimelineList(result);
             } else {
-                console.log("no timelines")
-            };
+                console.log("no timelines");
+            }
         });
     });
 
     // Timeline dropdown options
-	const timelineOptions = timelineList.map(timeline => <option key={timeline.id} value={timeline.id}>{timeline.name}</option>)
+    const timelineOptions = timelineList.map((timeline) => (
+        <option key={timeline.id} value={timeline.id}>
+            {timeline.name}
+        </option>
+    ));
 
     return (
         <select className="form-control" onChange={props.onChange} name={props.name}>
@@ -41,4 +46,4 @@ export default function TimelineSelect(props: TimelineSelectProps) {
             {timelineOptions}
         </select>
     );
-};
+}
