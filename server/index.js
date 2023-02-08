07ad25b6cddfb334b5f1be4dc8e33ff2e34c7e57
jon <20024680@myrp.edu.sg>
2023-02-08 17:47:49 +0800

    master delete

--- server/index.js
+++ server/index.js
@@ -96,9 +96,10 @@ app.prepare().then(() => {
 
     server.get("/api/asset/:plant_id", checkIfLoggedInAPI, controllers.asset.getAssetsFromPlant);
 
-  server.get("/api/master/new", checkIfLoggedInAPI, controllers.master.fetchMasterTypeEntry);
-  server.post("/api/master/new", checkIfLoggedInAPI, controllers.master.postMasterTypeEntry);
-  server.get("/api/master/:type", checkIfLoggedInAPI, controllers.master.fetchMasterInfo);
+    server.get("/api/master/new", checkIfLoggedInAPI, controllers.master.fetchMasterTypeEntry);
+    server.post("/api/master/new", checkIfLoggedInAPI, controllers.master.createMasterTypeEntry);
+    server.get("/api/master/:type", checkIfLoggedInAPI, controllers.master.fetchMasterInfo);
+    server.delete("/api/master/:type/:id", checkIfLoggedInAPI, controllers.master.deleteMasterTypeEntry);
     
     server.get("/api/schedule/:plant_id", checkIfLoggedInAPI, controllers.schedule.getViewSchedules);
     server.get("/api/getPlants", checkIfLoggedInAPI, controllers.schedule.getPlants);
