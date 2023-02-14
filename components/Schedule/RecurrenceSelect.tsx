fe38f91fc4b7e3247001781d0bae417594bfd1c7
zwe <zwezeya02@gmail.com>
2023-02-14 16:30:56 +0800

    schedule modal wip left assigned

--- components/Schedule/RecurrenceSelect.tsx
+++ components/Schedule/RecurrenceSelect.tsx
@@ -1,29 +1,53 @@
 import React, { useState, useEffect } from "react";
 
 interface RecurrenceSelectProps {
-    startDate: Date;
-    endDate: Date;
+    startDate?: Date;
+    endDate?: Date;
     name?: string;
     onChange: React.ChangeEventHandler
 }
 
+interface recurrencePeriod {
+    label: string;
+    value: number;
+}
 
-const recurrencePeriodList = ["Daily", "Weekly", "Fortnightly", "Monthly", "Quarterly", "Semi-Annually", "Yearly"];
+const recurrencePeriodList: recurrencePeriod[] = [{label: "Daily", value: 1}, 
+    {label: "Weekly", value: 7},
+    {label: "Fortnightly", value: 14},
+    {label: "Monthly", value: 30},
+    {label: "Quarterly", value: 90},
+    {label: "Semi-Annually", value: 180},
+    {label: "Yearly", value: 360},
+];
 
 export default function RecurrenceSelect(props: RecurrenceSelectProps) {
-    const [validPeriods, setValidPeriods] = useState([]);
+    const [validPeriods, setValidPeriods] = useState<recurrencePeriod[]>([]);
 
     useEffect(() => {
-        if (!props.startDate || !props.endDate) setValidPeriods([]);
+        if ((!props.startDate || !props.endDate) || (props.startDate > props.endDate)) setValidPeriods([]);
         else {
-
+            setValidPeriods([]);
+            // Calculate the difference in dats
+            const days = (Date.UTC(props.endDate.getFullYear(), props.endDate.getMonth(), props.endDate.getDate())
+             - Date.UTC(props.startDate.getFullYear(), props.startDate.getMonth(), props.startDate.getDate())) 
+             / (24*60*60*1000) + 1;
+            
+            // Add the relevant recurrence period to the state  
+            recurrencePeriodList.forEach(period => {
+                if (days > period.value) setValidPeriods(prev => [...prev, period])
+            })
         }
-
     }, [props.startDate, props.endDate])
 
-    return (
-        <select>
+    const options = validPeriods.map(period => {
+        return <option key={period.value} value={period.value} >{period.label}</option>
+    })
 
+    return (
+        <select className="form-select" onChange={props.onChange} name={props.name} >
+            <option hidden>Select a Recurrence Period</option>
+            {options}
         </select>
     )
 };
