fecc8195b354f6df9c8423392b90889106f5e6da
calebyyy <caleb.ng.yy@gmail.com>
2023-03-28 10:31:15 +0800

    Created Select System Asset

---
+++ components/Asset/SelectSystemAsset.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import ModuleSelect, { ModuleSelectOption } from '../ModuleLayout/ModuleSelect';
+import axios from "axios";
+
+async function getSystems() : Promise<ModuleSelectOption[]> {
+    return await axios.get("/api/master/system_asset_lvl5")
+        .then(res => {
+            const uniqueValues = new Set();
+            return res.data.rows.reduce((acc: ModuleSelectOption[], plant_system_assets: any) => {
+                if (!uniqueValues.has(plant_system_assets.system_asset_lvl5)) {
+                    uniqueValues.add(plant_system_assets.system_asset_lvl5);
+                    acc.push({id: plant_system_assets.system_asset_lvl5, option: plant_system_assets.system_asset_lvl5});
+                }
+                return acc;
+            }, []);
+        })
+}
+
+export default function SystemSelect() {
+    const [systemOptions, setSystemOptions] = useState<ModuleSelectOption[]>([]);
+
+    useEffect(() => {
+        getSystems().then(result => {
+            setSystemOptions(result);
+        });
+    })
+
+    return (
+        <ModuleSelect optionData={systemOptions} placeholder="Select system Asset" />
+    )
+}
