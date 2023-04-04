3dc4500a551d95702bde7e6bd479cc3f0c768e62
zwe <zwezeya02@gmail.com>
2023-04-04 11:09:05 +0800

    changed chklist queries

--- server/controllers/checklist.js
+++ server/controllers/checklist.js
@@ -2,41 +2,51 @@ const db = require("../../db");
 const { generateCSV } = require("../csvGenerator");
 const moment = require("moment");
 
-const fetchTemplateChecklistsQuery = `SELECT cl.checklist_id, cl.chl_name, cl.description, cl.status_id,
-concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
-concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
-concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
-pm.plant_name,
-pm.plant_id,
-completeremarks_req,
-tmp1.assetNames AS linkedassets,
-linkedassetids,
-cl.chl_type,
-cl.created_date,
-cl.history,
-st.status
-FROM keppel.users u
-JOIN keppel.user_access ua ON u.user_id = ua.user_id
-JOIN keppel.checklist_master cl on ua.allocatedplantids LIKE concat(concat('%',cl.plant_id::text), '%')
-LEFT JOIN (
-SELECT t3.checklist_id, string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text ORDER BY t2.psa_id ASC) AS assetNames
-FROM  keppel.system_assets AS t1 ,keppel.plant_system_assets AS t2, keppel.checklist_master AS t3
-WHERE t1.system_asset_id = t2.system_asset_id_lvl4 AND t3.linkedassetids LIKE concat(concat('%',t2.psa_id::text) , '%')
-	GROUP BY t3.checklist_id) tmp1 ON tmp1.checklist_id = cl.checklist_id
-LEFT JOIN keppel.users assignU ON assignU.user_id = cl.assigned_user_id
-LEFT JOIN keppel.users createdU ON createdU.user_id = cl.created_user_id
-LEFT JOIN keppel.users signoff ON signoff.user_id = cl.signoff_user_id
-LEFT JOIN keppel.plant_master pm ON pm.plant_id = cl.plant_id
-JOIN keppel.status_cm st ON st.status_id = cl.status_id
-		
-WHERE ua.user_id = $1
-AND (cl.chl_type = 'Template')
-AND (cl.status_id is null or cl.status_id = 1 or cl.status_id = 2 or cl.status_id = 3 or cl.status_id = 6)
-ORDER BY cl.checklist_id DESC;
-`;
-
-const fetchTemplateChecklists = async (req, res, next) => {
-    db.query(fetchTemplateChecklistsQuery, [req.user.id], (err, result) => {
+const fetchPendingChecklists = async (req, res, next) => {
+    db.query(`
+    SELECT 
+        cl.checklist_id, 
+        cl.chl_name, 
+        cl.description, 
+        cl.status_id,
+        concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
+        concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
+        concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
+        pm.plant_name,
+        pm.plant_id,
+        completeremarks_req,
+        tmp1.assetNames AS linkedassets,
+        linkedassetids,
+        cl.chl_type,
+        cl.created_date,
+        cl.history,
+        st.status
+    FROM 
+        keppel.users u
+        JOIN keppel.user_access ua ON u.user_id = ua.user_id
+        JOIN keppel.checklist_master cl on ua.allocatedplantids LIKE concat(concat('%',cl.plant_id::text), '%')
+        LEFT JOIN (
+            SELECT 
+                t3.checklist_id, 
+                string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text ORDER BY t2.psa_id ASC) AS assetNames
+            FROM  
+                keppel.system_assets AS t1,
+                keppel.plant_system_assets AS t2, 
+                keppel.checklist_master AS t3
+            WHERE 
+                t1.system_asset_id = t2.system_asset_id_lvl4 AND 
+                t3.linkedassetids LIKE concat(concat('%',t2.psa_id::text) , '%')
+            GROUP BY t3.checklist_id) tmp1 ON tmp1.checklist_id = cl.checklist_id
+        LEFT JOIN keppel.users assignU ON assignU.user_id = cl.assigned_user_id
+        LEFT JOIN keppel.users createdU ON createdU.user_id = cl.created_user_id
+        LEFT JOIN keppel.users signoff ON signoff.user_id = cl.signoff_user_id
+        LEFT JOIN keppel.plant_master pm ON pm.plant_id = cl.plant_id
+        JOIN keppel.status_cm st ON st.status_id = cl.status_id	
+    WHERE 
+        ua.user_id = $1 AND 
+        (cl.status_id is null or cl.status_id = 1 or cl.status_id = 6)
+    ORDER BY cl.checklist_id DESC;
+    `, [req.user.id], (err, result) => {
         if (err) return res.status(400).json({ msg: err });
         if (result.rows.length == 0) return res.status(201).json({ msg: "No checklist" });
 
@@ -46,30 +56,49 @@ const fetchTemplateChecklists = async (req, res, next) => {
 
 const fetchForReviewChecklists = async (req, res, next) => {
     db.query(
-        `SELECT cl.checklist_id, cl.chl_name, cl.description, cl.status_id,
-			concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
-			concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
-			concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
-			pm.plant_name,pm.plant_id,
-			completeremarks_req, tmp1.assetNames as linkedassets, cl.chl_type, cl.created_date, cl.history, st.status 
-			FROM  keppel.users u
-				JOIN keppel.user_access ua on u.user_id = ua.user_id
-				JOIN keppel.checklist_master cl on ua.allocatedplantids like concat(concat('%',cl.plant_id::text) , '%')
-				LEFT JOIN (
-					select  t3.checklist_id, string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text order by t2.psa_id asc) as assetNames
-						from  keppel.system_assets   as t1 ,keppel.plant_system_assets as t2, keppel.checklist_master as t3
-						where t1.system_asset_id = t2.system_asset_id_lvl4 and  t3.linkedassetids like concat(concat('%',t2.psa_id::text) , '%')
-							group by t3.checklist_id) tmp1 on tmp1.checklist_id = cl.checklist_id
-			left join keppel.users assignU on assignU.user_id = cl.assigned_user_id
-			left join keppel.users createdU on createdU.user_id = cl.created_user_id
-			left join keppel.users signoff on signoff.user_id = cl.signoff_user_id
-			left join keppel.plant_master pm on pm.plant_id = cl.plant_id 
-      JOIN keppel.status_cm st ON st.status_id = cl.status_id
-							
-			where ua.user_id = $1
-			and (cl.chl_type = 'Record')
-			and (cl.status_id = 2 or cl.status_id = 3 or cl.status_id = 4 or cl.status_id = 7)
-			order by cl.checklist_id desc;
+        `
+        SELECT 
+            cl.checklist_id, 
+            cl.chl_name, 
+            cl.description, 
+            cl.status_id,
+            concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
+            concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
+            concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
+            pm.plant_name,
+            pm.plant_id,
+            completeremarks_req, 
+            tmp1.assetNames AS linkedassets, 
+            cl.chl_type, 
+            cl.created_date, 
+            cl.history, 
+            st.status 
+        FROM  
+            keppel.users u
+            JOIN keppel.user_access ua ON u.user_id = ua.user_id
+            JOIN keppel.checklist_master cl ON ua.allocatedplantids LIKE concat(concat('%',cl.plant_id::text) , '%')
+            LEFT JOIN (
+                SELECT  
+                    t3.checklist_id, 
+                    string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text ORDER BY t2.psa_id ASC) AS assetNames
+                FROM  
+                    keppel.system_assets AS t1,
+                    keppel.plant_system_assets AS t2, 
+                    keppel.checklist_master AS t3
+                WHERE 
+                    t1.system_asset_id = t2.system_asset_id_lvl4 AND  
+                    t3.linkedassetids LIKE concat(concat('%',t2.psa_id::text) , '%')
+                GROUP BY t3.checklist_id) tmp1 ON tmp1.checklist_id = cl.checklist_id
+            LEFT JOIN keppel.users assignU ON assignU.user_id = cl.assigned_user_id
+            LEFT JOIN keppel.users createdU ON createdU.user_id = cl.created_user_id
+            LEFT JOIN keppel.users signoff ON signoff.user_id = cl.signoff_user_id
+            LEFT JOIN keppel.plant_master pm ON pm.plant_id = cl.plant_id 
+            JOIN keppel.status_cm st ON st.status_id = cl.status_id						
+        WHERE 
+            ua.user_id = $1 AND 
+            (cl.status_id = 2 OR cl.status_id = 3 OR cl.status_id = 4)
+        ORDER BY cl.checklist_id desc;
+                
 		`,
         [req.user.id],
         (err1, result) => {
@@ -93,30 +122,51 @@ const fetchForReviewChecklists = async (req, res, next) => {
 
 const fetchApprovedChecklists = async (req, res, next) => {
     db.query(
-        `SELECT cl.checklist_id, cl.chl_name, cl.description, cl.status_id,
-	concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
-	concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
-	concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
-	pm.plant_name,pm.plant_id,
-	completeremarks_req, tmp1.assetNames as linkedassets, cl.chl_type, cl.created_date, cl.history, st.status 
-	FROM  keppel.users u
-		JOIN keppel.user_access ua on u.user_id = ua.user_id
-		JOIN keppel.checklist_master cl on ua.allocatedplantids like concat(concat('%',cl.plant_id::text) , '%')
-		LEFT JOIN (
-			select  t3.checklist_id, string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text order by t2.psa_id asc) as assetNames
-				from  keppel.system_assets   as t1 ,keppel.plant_system_assets as t2, keppel.checklist_master as t3
-				where t1.system_asset_id = t2.system_asset_id_lvl4 and  t3.linkedassetids like concat(concat('%',t2.psa_id::text) , '%')
-					group by t3.checklist_id) tmp1 on tmp1.checklist_id = cl.checklist_id
-	  left join keppel.users assignU on assignU.user_id = cl.assigned_user_id
-	  left join keppel.users createdU on createdU.user_id = cl.created_user_id
-	  left join keppel.users signoff on signoff.user_id = cl.signoff_user_id
-	  left join keppel.plant_master pm on pm.plant_id = cl.plant_id 
-    JOIN keppel.status_cm st ON st.status_id = cl.status_id
-					
-	where ua.user_id = $1
-	and (cl.chl_type = 'Approved')
-	and (cl.status_id = 5 or cl.status_id = 7)
-	order by cl.checklist_id desc;`,
+        `
+        SELECT 
+            cl.checklist_id, 
+            cl.chl_name, 
+            cl.description, 
+            cl.status_id,
+            concat( concat(createdU.first_name ,' '), createdU.last_name ) AS createdByUser,
+            concat( concat(assignU.first_name ,' '), assignU.last_name ) AS assigneduser,
+            concat( concat(signoff.first_name ,' '), signoff.last_name ) AS signoffUser,  
+            pm.plant_name,
+            pm.plant_id,
+            completeremarks_req, 
+            tmp1.assetNames as linkedassets, 
+            cl.chl_type, 
+            cl.created_date, 
+            cl.history, 
+            st.status 
+        FROM 
+            keppel.users u 
+            JOIN keppel.user_access ua ON u.user_id = ua.user_id
+            JOIN keppel.checklist_master cl ON ua.allocatedplantids LIKE concat(concat('%',cl.plant_id::text) , '%')
+            LEFT JOIN (
+                SELECT  
+                    t3.checklist_id, 
+                    string_agg(concat( system_asset , ' | ' , plant_asset_instrument )::text, ', '::text ORDER BY t2.psa_id asc) as assetNames
+                FROM  
+                    keppel.system_assets AS t1,
+                    keppel.plant_system_assets AS t2, 
+                    keppel.checklist_master AS t3
+                WHERE 
+                    t1.system_asset_id = t2.system_asset_id_lvl4 AND  
+                    t3.linkedassetids LIKE concat(concat('%',t2.psa_id::text) , '%')
+                group by 
+                    t3.checklist_id) tmp1 ON tmp1.checklist_id = cl.checklist_id
+            LEFT JOIN keppel.users assignU ON assignU.user_id = cl.assigned_user_id
+            LEFT JOIN keppel.users createdU ON createdU.user_id = cl.created_user_id
+            LEFT JOIN keppel.users signoff ON signoff.user_id = cl.signoff_user_id
+            LEFT JOIN keppel.plant_master pm ON pm.plant_id = cl.plant_id 
+            JOIN keppel.status_cm st ON st.status_id = cl.status_id
+                            
+        WHERE 
+            ua.user_id = $1 AND 
+            (cl.status_id = 5 OR cl.status_id = 7)
+        ORDER BY cl.checklist_id DESC;
+    `,
         [req.user.id],
         (err, result) => {
             if (err) {
@@ -191,7 +241,7 @@ const createNewChecklistRecord = async (req, res, next) => {
         checklist.signoff_user_id,
         checklist.linkedassetids,
         checklist.datajson,
-        "record",
+        "Record",
         checklist.plant_id,
         today,
         req.user.id,
@@ -234,7 +284,7 @@ const createNewChecklistTemplate = async (req, res, next) => {
         checklist.description,
         checklist.signoff_user_id,
         checklist.datajson,
-        "template",
+        "Template",
         checklist.plant_id,
         today,
         req.user.id,
@@ -293,7 +343,7 @@ const createChecklistCSV = async (req, res, next) => {
 };
 
 module.exports = {
-    fetchTemplateChecklists,
+    fetchPendingChecklists,
     fetchForReviewChecklists,
     fetchApprovedChecklists,
     fetchChecklistTemplateNames,
