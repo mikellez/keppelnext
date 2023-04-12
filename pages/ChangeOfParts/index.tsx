35aedb86c7005ae4b1ca0539af382851bafb4ab7
zwe <zwezeya02@gmail.com>
2023-04-12 12:39:22 +0800

    add cop before moving to branch

---
+++ pages/ChangeOfParts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ModuleMain, ModuleHeader, ModuleContent } from '../../components'
+
+const ChangeOfPartsPage = () => {
+    return (
+        <ModuleMain>
+            <ModuleHeader header="Change of Parts"></ModuleHeader>
+            <ModuleContent>
+                
+            </ModuleContent>
+        </ModuleMain>
+    );
+};
+
+export default ChangeOfPartsPage;
