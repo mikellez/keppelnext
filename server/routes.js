2294c6b3df15c8bf60c8a22059ad92445fc8e0fb
zwe <zwezeya02@gmail.com>
2023-04-13 09:52:08 +0800

    bug fix for cop backend

--- server/routes.js
+++ server/routes.js
@@ -333,7 +333,7 @@ router.get("/schedule/event/:id", checkIfLoggedInAPI, controllers.schedule.getSc
 router.get("/activity/account_log", checkIfLoggedInAPI, controllers.activity.getEventtHistory);
 router.get("/activity/csv", checkIfLoggedInAPI, controllers.activity.createActivityCSV);
 
-router.get("/changeOfParts", checkIfLoggedInAPI, controllers.changeOfParts.fetchChangeOfParts);
+router.get("/changeOfParts/:plant_id?", checkIfLoggedInAPI, controllers.changeOfParts.fetchChangeOfParts);
 
 // NO API ROUTE
 router.all("/*", (req, res) => {
