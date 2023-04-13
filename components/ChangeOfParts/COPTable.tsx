c903f068fbcb865e7b3f64b2da9a4fcdea26da22
zwe <zwezeya02@gmail.com>
2023-04-13 10:05:10 +0800

    fix ssr for cop

---
+++ components/ChangeOfParts/COPTable.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+    Table,
+    Header,
+    HeaderRow,
+    Body,
+    Row,
+    HeaderCell,
+    Cell,
+  } from '@table-library/react-table-library/table';
+
+const COPTable = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default COPTable;
