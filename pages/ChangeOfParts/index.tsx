3943861ba54960ec3875e37514d30b22ad10beb0
zwe <zwezeya02@gmail.com>
2023-04-13 11:57:38 +0800

    add cop table

--- pages/ChangeOfParts/index.tsx
+++ pages/ChangeOfParts/index.tsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ModuleMain, ModuleHeader, ModuleContent } from '../../components'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import { CMMSChangeOfParts } from '../../types/common/interfaces';
 import PlantSelect from '../../components/PlantSelect';
 import axios from 'axios';
+import COPTable from '../../components/ChangeOfParts/COPTable';
 
-interface ChangeOfPartsPageProps {
+export interface ChangeOfPartsPageProps {
     changeOfParts: CMMSChangeOfParts[];
 };
 
+const fetchChangeOfParts = async (plantId: number) => {
+    const plant = plantId > 0 ? plantId : "";
+    return await axios.get<CMMSChangeOfParts[]>("/api/changeOfParts/" + plant)   
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
 const ChangeOfPartsPage = (props: ChangeOfPartsPageProps) => {
-    
+
+    const [COPData, setCOPData] = useState<CMMSChangeOfParts[]>(props.changeOfParts);
+
+    const updateCOPData = async (plantId: number) => {
+        const newCOP = await fetchChangeOfParts(plantId);
+        if (newCOP) setCOPData(newCOP);
+        else setCOPData([]);
+    };
+
+    console.log(COPData)
+
     return (
         <ModuleMain>
             <ModuleHeader header="Change of Parts">
                 <PlantSelect 
-                    onChange={() => {}}
+                    onChange={(e) => updateCOPData(+e.target.value)}
+                    allPlants
+                    accessControl
                 />
             </ModuleHeader>
             <ModuleContent>
-                
+                <COPTable changeOfParts={COPData} />
             </ModuleContent>
         </ModuleMain>
     );
