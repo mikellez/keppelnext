7abe7f75cb38211e292b1312095a23143b8f62dc
Raghava <chinnu4148@gmail.com>
2023-04-28 15:37:12 +0800

    npm script runner

--- server/userAuth.js
+++ server/userAuth.js
@@ -1,43 +1,66 @@
-
 const db = require("../db");
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const bcrypt = require("bcryptjs");
-const session = require('express-session')
+const session = require("express-session");
 
 module.exports = (server) => {
-    server.use(session({
-        secret: "secret",
-        resave: true,
-        saveUninitialized: false
-    }))
-    server.use(passport.initialize());
-    server.use(passport.session());
+  server.use(
+    session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: false,
+    })
+  );
+  server.use(passport.initialize());
+  server.use(passport.session());
 
-    passport.use(new LocalStrategy((username, password, callback) => {
-        console.log(username, password)
-        db.query(`SELECT * FROM keppel.users AS u JOIN keppel.user_access AS ua
-        ON u.user_id = ua.user_id WHERE LOWER(user_name) = LOWER($1::text)`, [username], (err, result) => {
-            if(err)							return callback(err);
-            if(result.rows.length < 1)		return callback(null, false, { message: 'Incorrect username or password.' });
+  passport.use(
+    new LocalStrategy((username, password, callback) => {
+      console.log(username, password);
+      db.query(
+        `SELECT * FROM keppel.users AS u JOIN keppel.user_access AS ua
+        ON u.user_id = ua.user_id WHERE LOWER(ua.user_name) = LOWER($1::text)`,
+        [username],
+        (err, result) => {
+          if (err) return callback(err);
+          if (result.rows.length < 1)
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
 
-            bcrypt.compare(password, result.rows[0].user_pass.toString(), (bErr, bRes) => {
-                console.log(bErr);
-                console.log(bRes);
+          bcrypt.compare(
+            password,
+            result.rows[0].user_pass.toString(),
+            (bErr, bRes) => {
+              console.log(bErr);
+              console.log(bRes);
 
-                if (bErr)		return callback(null, false, { message: 'Incorrect username or password.', detailedmsg: bErr });
-                if(!bRes)		return callback(null, false, { message: 'Incorrect username or password.' });
-                                console.log("success"); return callback(null, result.rows[0]);
-            })
-        })
-    }));
+              if (bErr)
+                return callback(null, false, {
+                  message: "Incorrect username or password.",
+                  detailedmsg: bErr,
+                });
+              if (!bRes)
+                return callback(null, false, {
+                  message: "Incorrect username or password.",
+                });
+              console.log("success");
+              return callback(null, result.rows[0]);
+            }
+          );
+        }
+      );
+    })
+  );
 
-    passport.serializeUser((user, cb) => {
-        cb(null, user.user_id);
-    });
+  passport.serializeUser((user, cb) => {
+    cb(null, user.user_id);
+  });
 
-    passport.deserializeUser((id, cb) => {
-        db.query(`SELECT 
+  passport.deserializeUser((id, cb) => {
+    db.query(
+      `SELECT 
                 employee_id,
                 user_id,
                 first_name,
@@ -48,18 +71,23 @@ module.exports = (server) => {
             FROM 
                 keppel.user_access 
             WHERE 
-                user_id = $1::integer`, [id], (err, result) => {
-            if (err) return cb(err);
+                user_id = $1::integer`,
+      [id],
+      (err, result) => {
+        if (err) return cb(err);
 
-            const userInfo = {
-                employee_id: result.rows[0].employee_id,
-                id: result.rows[0].user_id,
-                name: result.rows[0].first_name + " " + result.rows[0].last_name,
-                role_id: parseInt(result.rows[0].role_id),
-                role_name: result.rows[0].role_name,
-                allocated_plants: result.rows[0].allocated_plants.map(plant => parseInt(plant)).sort()
-            };
-            cb(null, userInfo);
-        })
-    })
-}
+        const userInfo = {
+          employee_id: result.rows[0].employee_id,
+          id: result.rows[0].user_id,
+          name: result.rows[0].first_name + " " + result.rows[0].last_name,
+          role_id: parseInt(result.rows[0].role_id),
+          role_name: result.rows[0].role_name,
+          allocated_plants: result.rows[0].allocated_plants
+            .map((plant) => parseInt(plant))
+            .sort(),
+        };
+        cb(null, userInfo);
+      }
+    );
+  });
+};
