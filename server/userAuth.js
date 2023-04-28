578f27081b0ff12ff8c0f986047235e0b847e0e6
Raghava <chinnu4148@gmail.com>
2023-04-28 15:38:48 +0800

    ed

--- server/userAuth.js
+++ server/userAuth.js
@@ -15,12 +15,19 @@ module.exports = (server) => {
   server.use(passport.initialize());
   server.use(passport.session());
 
-    passport.use(new LocalStrategy((username, password, callback) => {
-        console.log(username, password)
-        db.query(`SELECT * FROM keppel.users AS u JOIN keppel.user_access AS ua
-        ON u.user_id = ua.user_id WHERE LOWER(ua.user_name) = LOWER($1::text)`, [username], (err, result) => {
-            if(err)							return callback(err);
-            if(result.rows.length < 1)		return callback(null, false, { message: 'Incorrect username or password.' });
+  passport.use(
+    new LocalStrategy((username, password, callback) => {
+      console.log(username, password);
+      db.query(
+        `SELECT * FROM keppel.users AS u JOIN keppel.user_access AS ua
+        ON u.user_id = ua.user_id WHERE LOWER(ua.user_name) = LOWER($1::text)`,
+        [username],
+        (err, result) => {
+          if (err) return callback(err);
+          if (result.rows.length < 1)
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
 
           bcrypt.compare(
             password,
@@ -51,16 +58,11 @@ module.exports = (server) => {
     cb(null, user.user_id);
   });
 
-<<<<<<< HEAD
   passport.deserializeUser((id, cb) => {
     db.query(
       `SELECT 
-=======
-    passport.deserializeUser((id, cb) => {
-        db.query(`SELECT 
                 user_name,
                 user_email,
->>>>>>> 897fd3003895de6258b0a5e7addffe28acd96723
                 employee_id,
                 user_id,
                 first_name,
@@ -76,7 +78,6 @@ module.exports = (server) => {
       (err, result) => {
         if (err) return cb(err);
 
-<<<<<<< HEAD
         const userInfo = {
           employee_id: result.rows[0].employee_id,
           id: result.rows[0].user_id,
@@ -86,29 +87,14 @@ module.exports = (server) => {
           allocated_plants: result.rows[0].allocated_plants
             .map((plant) => parseInt(plant))
             .sort(),
+          email: result.rows[0].user_email,
+          username: result.rows[0].user_name,
+          first_name: result.rows[0].first_name,
+          last_name: result.rows[0].last_name,
         };
+        // console.log(userInfo)
         cb(null, userInfo);
       }
     );
   });
 };
-=======
-            const userInfo = {
-                employee_id: result.rows[0].employee_id,
-                id: result.rows[0].user_id,
-                name: result.rows[0].first_name + " " + result.rows[0].last_name,
-                role_id: parseInt(result.rows[0].role_id),
-                role_name: result.rows[0].role_name,
-                allocated_plants: result.rows[0].allocated_plants.map(plant => parseInt(plant)).sort(),
-                email: result.rows[0].user_email,
-                username: result.rows[0].user_name,
-                first_name: result.rows[0].first_name,
-                last_name: result.rows[0].last_name
-
-            };
-            // console.log(userInfo)
-            cb(null, userInfo);
-        })
-    })
-}
->>>>>>> 897fd3003895de6258b0a5e7addffe28acd96723
