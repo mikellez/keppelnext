802d1b8bb28e5f80ef1659a5011439d5d40206da
Raghava <chinnu4148@gmail.com>
2023-05-09 10:52:28 +0800

    sdc

--- axios.config.js
+++ axios.config.js
@@ -1,27 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3002'
+  baseURL: "http://localhost:3001",
 });
 
-instance.interceptors.request.use(
-  (config) => {
+instance.interceptors.request.use((config) => {
+  config.withCredentials = true;
+  return config;
+});
 
-    config.withCredentials = true;
-    return config;
-  }
-);
-
-instance.interceptors.response.use((response) => {
-  console.log(`response ${response}`);
-  return response;
-}, (error) => {
-
+instance.interceptors.response.use(
+  (response) => {
+    console.log(`response ${response}`);
+    return response;
+  },
+  (error) => {
     const statusCode = error.response ? error.response.status : null;
     if (statusCode === 401) {
       window.location = "/Login";
     }
-    return Promise.reject(error); 
-});
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
