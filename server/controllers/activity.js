0ddb09ece6b6f04d7afb4b63dd667249f6a217c4
calebyyy <caleb.ng.yy@gmail.com>
2023-05-17 10:13:13 +0800

    Changed activity log

--- server/controllers/activity.js
+++ server/controllers/activity.js
@@ -1,64 +1,42 @@
 const db = require("../../db");
 const { generateCSV } = require("../csvGenerator");
+const  tableInfo = require("../../public/master.json");
+const activityLog = require("../../public/activity.json");
 
-const query = 
-        ` SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Request'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.request,
-        LATERAL jsonb_array_elements(request.activity_log) activity(value)
+let query = `
+    SELECT concat(ii.first_name, ' ', ii.last_name) AS user_name,
+            'Login events'::text AS type,
+            to_timestamp(substr(i.datetime::text, 1, length(i.datetime::text) - 3), 'mm-dd-yyyy HH24:MI'::text) AS event_time,
+            i.activity AS description
+        FROM keppel.loginevents i,
+            keppel.users ii
+        WHERE i.userid = ii.user_id
+        GROUP BY (to_timestamp(substr(i.datetime::text, 1, length(i.datetime::text) - 3), 'mm-dd-yyyy HH24:MI'::text)), ii.user_name, i.activity, ii.first_name, ii.last_name
+       
+`
+
+for (const key in activityLog){
+    query += `
     UNION ALL
-     SELECT concat(ii.first_name, ' ', ii.last_name) AS user_name,
-        'Login events'::text AS type,
-        to_timestamp(substr(i.datetime::text, 1, length(i.datetime::text) - 3), 'mm-dd-yyyy HH24:MI'::text) AS event_time,
-        i.activity AS description
-       FROM keppel.loginevents i,
-        keppel.users ii
-      WHERE i.userid = ii.user_id
-      GROUP BY (to_timestamp(substr(i.datetime::text, 1, length(i.datetime::text) - 3), 'mm-dd-yyyy HH24:MI'::text)), ii.user_name, i.activity, ii.first_name, ii.last_name
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Checklist'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'yyyy-mm-dd HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.checklist_master,
-        LATERAL jsonb_array_elements(checklist_master.activity_log) activity(value)
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Assets'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.plant_system_assets,
-        LATERAL jsonb_array_elements(plant_system_assets.activity_log) activity(value)
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
+    SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
+    '${key}'::text AS type,
+    to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), '${activityLog[key].format}') AS event_time,
+    btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
+    FROM ${activityLog[key].table},
+    LATERAL jsonb_array_elements(${activityLog[key].table}.activity_log) activity(value)
+    `
+}
+
+for (const key in tableInfo) {
+    query += ` UNION ALL
+    SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
         'Master'::text AS type,
         to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
         btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.plant_master,
-        LATERAL jsonb_array_elements(plant_master.activity_log) activity(value)
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Master'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.system_master,
-        LATERAL jsonb_array_elements(system_master.activity_log) activity(value)
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Master'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.asset_type,
-        LATERAL jsonb_array_elements(asset_type.activity_log) activity(value)
-    UNION ALL
-     SELECT btrim(((activity.value -> 'name'::text)::character varying)::text, '"'::text) AS user_name,
-        'Master'::text AS type,
-        to_timestamp(substr(((activity.value -> 'date'::text)::character varying)::text, 2, length(((activity.value -> 'date'::text)::character varying)::text) - 5), 'dd-mm-yyyy HH24:MI'::text) AS event_time,
-        btrim(concat(activity.value -> 'activity'::text), '"'::text) AS description
-       FROM keppel.fault_types,
-        LATERAL jsonb_array_elements(fault_types.activity_log) activity(value)`
+         FROM keppel.${tableInfo[key].internalName},
+        LATERAL jsonb_array_elements(${tableInfo[key].internalName}.activity_log) activity(value)`
+}
+
 
 const getEventtHistory = async (req, res, next) => {
     let q = `SELECT * FROM (${query}) AS activity_log WHERE event_time >= date_trunc('month', CURRENT_DATE) ORDER BY event_time DESC;`
