cdb8557a3f6849185ab639b35dfbbf3b4ab852b6
Raghava <chinnu4148@gmail.com>
2023-06-06 15:38:14 +0800

    fixed duplication

--- pages/Checklist/index.tsx
+++ pages/Checklist/index.tsx
@@ -178,7 +178,7 @@ export default function Checklist(props: ChecklistProps) {
             })
           );
           setTotalPages(response.data.total);
-          setReady(true);
+            setReady(true);
         })
         .catch((e) => {
           setChecklistItems([]);
