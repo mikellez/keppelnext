d65064923e85cafa2a901fb348dc558ca6b76e85
zwe <zwezeya02@gmail.com>
2023-06-12 16:44:26 +0800

    fix checklist rejection to return to assign instead of reassign

--- server/controllers/checklist.js
+++ server/controllers/checklist.js
@@ -762,7 +762,7 @@ const rejectChecklist = async (req, res, next) => {
         UPDATE
             keppel.checklist_master
         SET 
-            status_id = 3,
+            status_id = 2,
             history = concat(history,'${updatehistory}'),
             activity_log = activity_log || $1
         WHERE 
