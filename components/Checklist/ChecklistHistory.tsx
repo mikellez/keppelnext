9ddf63c2f7d2735c042a6602aabdc39c5bb192d2
Raghava <chinnu4148@gmail.com>
2023-06-16 11:08:39 +0800

    date format

--- components/Checklist/ChecklistHistory.tsx
+++ components/Checklist/ChecklistHistory.tsx
@@ -1,45 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 interface ChecklistHistoryProps {
-    history: {[key : string]: string}[];
+  history: { [key: string]: string }[];
 }
 
 function rowElements(rows: { [key: string]: string }[]) {
-    return <>
-    {rows.map(row => {
-        console.log(row)
-        return <tr key={row["date"] + row["name"] + row["role"]}>
-        <td>{row["activity_type"]}</td>
-        <td>{row["activity"]}</td>
-        <td>{row["date"]}</td>
-        <td>{row["name"]}</td>
-    </tr>
-    })} 
+  return (
+    <>
+      {rows.map((row) => {
+        console.log(row);
+        return (
+          <tr key={row["date"] + row["name"] + row["role"]}>
+            <td>{row["activity_type"]}</td>
+            <td>{row["activity"]}</td>
+            <td>
+              {new Date(row["date"]).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              })}
+            </td>
+            <td>{row["name"]}</td>
+          </tr>
+        );
+      })}
     </>
+  );
 }
 
 const ChecklistHistory = (props: ChecklistHistoryProps) => {
+  const [rows, setRows] = useState<{ [key: string]: string }[]>([]);
 
-    
-    const [rows, setRows] = useState<{ [key: string]: string }[]>([]);
-
-    return (
-        <div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Status</th>
-                        <th>Action</th>
-                        <th>Date</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rowElements(props.history)}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Status</th>
+            <th>Action</th>
+            <th>Date</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>{rowElements(props.history)}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ChecklistHistory;
