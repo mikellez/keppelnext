f5d5a02487affdde601857b4c62d7475abcd3a7c
wenjj2000 <wenjj2000@gmail.com>
2023-07-12 17:04:27 +0800

    documentation for feedback validation

--- components/Feedback/FeedbackValidation.tsx
+++ components/Feedback/FeedbackValidation.tsx
@@ -1,3 +1,16 @@
+/**
+ * This component helps to add the required icon to any input lable
+ * 
+ * props: {
+ *  value : any
+ *  label : any
+ * }
+ * 
+ *  - value : any, the placeholder value on the input tab
+ *  - label : any, the name of the label 
+
+ */
+
 import React from "react";
 import CMMSContact from "../../types/common/interfaces";
 import RequiredIcon from "../RequiredIcon";
