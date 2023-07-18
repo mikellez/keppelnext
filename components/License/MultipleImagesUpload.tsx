ec4a040fe764f683a59c7fcee1145f7fdcb7be45
Zhetao <quzhetao2001@gmail.com>
2023-07-18 17:00:10 +0800

    Completed frontend for create license

---
+++ components/License/MultipleImagesUpload.tsx
@@ -0,0 +1,146 @@
+import React, {useEffect, useState, CSSProperties, useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+import RequiredIcon from '../RequiredIcon';
+import { CMMSLicense } from '../../pages/License/Form';
+
+const thumbsContainer: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumbsStyle: CSSProperties = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+
+const thumbInner: CSSProperties = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img: CSSProperties = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
+interface MultipleImagesUploader  {
+    setLicenseForm: React.Dispatch<React.SetStateAction<CMMSLicense>>;
+    isSubmitting: boolean;
+}
+
+
+
+const MultipleImagesUpload = (props: MultipleImagesUploader) => {
+    
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (props.isSubmitting) {
+      for (const image of images) {
+        URL.revokeObjectURL(image);
+      }
+    }
+  }, [props.isSubmitting]);    
+
+  
+
+  const handleImages = (files: File[]) => {
+    props.setLicenseForm(prev => {
+      return {
+        ...prev,
+        "images": [...prev.images, ...files]
+      }
+    })
+  }
+  
+  const deleteImage = (images: string, num: number) => {
+    console.log(images)
+    setImages(prev => {
+      const arr = prev;
+      return arr.filter((img, index) => {
+        // console.log(index)
+        return img !== images
+      })
+    })
+    props.setLicenseForm(prev => {
+      return {
+        ...prev,
+        "images": [...prev.images].filter((img, index) => index !== num)
+      };
+    })
+    // props.handleDelete(num);
+  }
+
+  const dropHandler = useCallback((acceptedFiles: File[]) => {
+    if (acceptedFiles.length < 1) {
+        return
+    }
+    handleImages(acceptedFiles);
+    setImages(prev => {
+      return [...prev, ...acceptedFiles.map(file => URL.createObjectURL(file))]
+    })
+    console.log("Handling drop")
+  }, [])
+
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: {
+    'image/*': []
+    },
+    onDrop: dropHandler
+  });
+
+  const thumbs = images.map((image, index) => (
+    <div style={thumbsStyle} key={index} onDoubleClick={() => deleteImage(image, index)}>
+      <div style={thumbInner}>
+        <img
+          src={image}
+          style={img}
+          alt=""
+        />
+      </div>
+    </div>
+  ));
+
+
+    return (
+      <section className="mb-3">
+          <label className="mb-3"> <RequiredIcon /> Attachment images</label>
+          <div style={{
+              flex: 1,
+              display: 'flex',
+              // flexDirection: 'column',
+              alignItems: 'center',
+              padding: '20px',
+              height: "10rem",
+              borderWidth: 5,
+              borderRadius: 10,
+              borderColor: '#eeeeee',
+              borderStyle: 'dashed',
+              backgroundColor: 'white',
+              color: '#bdbdbd',
+              outline: 'none',
+              transition: 'border .24s ease-in-out',
+              textAlign: 'center'
+              }} {...getRootProps({className: 'dropzone'})}>
+          <input {...getInputProps()} accept="image/*"/>
+          <p>Drag and drop some images here, or click to select images. Delete images by double clicking on the images</p>
+        </div>
+        <aside style={thumbsContainer}>
+          {thumbs}
+        </aside>
+      </section>
+    );
+}
+
+export default MultipleImagesUpload;
--- pages/License/Form/index.tsx
+++ pages/License/Form/index.tsx
@@ -7,8 +7,12 @@ import RequiredIcon from '../../../components/RequiredIcon';
 import ModuleSelect from '../../../components/ModuleLayout/ModuleSelect';
 import LicenseTypeSelect from '../../../components/License/LicenseTypeSelect';
 import PlantLocSelect from '../../../components/License/PlantLocSelect';
-import AssignToSelect from '../../../components/Schedule/AssignToSelect';
-import AssetSelect from '../../../components/Checklist/AssetSelect';
+import AssignToSelect, { AssignedUserOption } from '../../../components/Schedule/AssignToSelect';
+import AssetSelect, { AssetOption } from '../../../components/Checklist/AssetSelect';
+import MultipleImagesUpload from '../../../components/License/MultipleImagesUpload';
+import { SingleValue, MultiValue } from 'react-select';
+import { useForm } from 'react-hook-form'
+
 
 
 interface LicenseProps {
@@ -30,14 +34,16 @@ export interface CMMSLicenseType {
 }
 
 export interface CMMSLicense {
-    license_id?: number,
-    license_name: string,
-    license_provider: string,
-    license_type_id: number,
-    license_details: string,
-    plant_id: number,
-    plantLoc_id: number,
-    linked_asset_id: number,
+    license_id?: number;
+    license_name: string;
+    license_provider: string;
+    license_type_id: number;
+    license_details: string;
+    plant_id: number;
+    plantLoc_id: number;
+    linked_asset_id: number;
+    assigned_user_id: number;
+    images: File[];
 
 }
 
@@ -52,10 +58,21 @@ const LicenseNew = (props: LicenseProps) => {
         plant_id: -1,
         plantLoc_id: -1,
         linked_asset_id: - 1,
+        assigned_user_id: -1,
+        images: [],
     })
 
-    // console.log(props);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
     const router = useRouter();
+    // const { data: assetList } = useAsset(licenseForm.plant_id == -1 ? null : licenseForm.plant_id);
+
+    // update assign_to dropdown according to plant_id
+    // useEffect(() => {
+    //     if (licenseForm.plant_id != -1) {
+    //         instance.
+    //     }
+    // }, [licenseForm.plant_id])
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
         setLicenseForm(prev => {
@@ -75,6 +92,33 @@ const LicenseNew = (props: LicenseProps) => {
                 ...prev,
                 "plant_id": +plant_id,
                 "plantLoc_id": +loc_id,
+            }
+        })
+    }
+
+    const handleLinkedAsset = (option: MultiValue<AssetOption> | SingleValue<AssetOption>) => {
+        setLicenseForm(prev => {
+            return {
+                ...prev,
+                "linked_asset_id": (option as SingleValue<AssignedUserOption>)?.value as number
+            }
+        })
+    }
+
+    const handleAssignee = (option: MultiValue<AssignedUserOption> | SingleValue<AssignedUserOption>) => {
+        setLicenseForm(prev => {
+            return {
+                ...prev,
+                "assigned_user_id": (option as SingleValue<AssignedUserOption>)?.value as number
+            }
+        })
+    }
+
+    const handleImages = (files: File[]) => {
+        setLicenseForm(prev => {
+            return {
+                ...prev,
+                "images": [...prev.images, ...files]
             }
         })
     }
@@ -109,13 +153,28 @@ const LicenseNew = (props: LicenseProps) => {
                             value={licenseForm.license_details} onChange={handleInput}/>
                     </div>
                     <PlantLocSelect optionsData={props.plantLocs} onChange={handlePlantSelect}/>
-                    {/* <AssignToSelect /> */}
+                    <div className="mb-3">
+                        <label className="form-label"><RequiredIcon/>Linked Asset</label>
+                        <AssetSelect isSingle plantId={licenseForm.plant_id == -1 ? 1: licenseForm.plant_id}
+                        defaultIds={[]} onChange={handleLinkedAsset}/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label"><RequiredIcon/> Assign To</label>
+                        <AssignToSelect isSingle plantId={licenseForm.plant_id == -1 ? 1: licenseForm.plant_id}
+                            onChange={handleAssignee} disabled={licenseForm.plant_id === -1}/>
+                    </div>
+                    
                 </div>
                 <div className="col-6 ps-5">
-                    bye
+                <MultipleImagesUpload setLicenseForm={setLicenseForm} isSubmitting={isSubmitting}/>
+                
                 </div>
             </div>
+            
         </ModuleContent>
+        <div className='d-flex justify-content-end'>
+            <button className="btn btn-primary d-flex">Submit</button>
+        </div>
     </ModuleMain>
 }
 
