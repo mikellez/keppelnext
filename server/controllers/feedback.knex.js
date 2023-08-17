7e905a1fd11ee1cd5f3260fbab0ecb7f7c3dbb06
wenjj2000 <wenjj2000@gmail.com>
2023-08-17 14:25:00 +0800

    changed saved feedback csv format to .csv

--- server/controllers/feedback.knex.js
+++ server/controllers/feedback.knex.js
@@ -424,7 +424,7 @@ const createFeedbackCSV = async (req, res, next) => {
   //   console.log(req.body);
   data = req.body;
   const today = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-  const fileName = moment(today).format("YYYY-MM-DD_HH-mm-ss");
+  const fileName = moment(today).format("YYYY-MM-DD_HH-mm-ss") + ".csv";
   //   console.log(fileName);
 
   const activity_log = [
