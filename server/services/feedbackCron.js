b33b8aa174bb0c053e36661fe8bf31a88d0d8438
wenjj2000 <wenjj2000@gmail.com>
2023-08-21 15:47:43 +0800

    rsync feedback jj

--- server/services/feedbackCron.js
+++ server/services/feedbackCron.js
@@ -5,7 +5,9 @@ const moment = require("moment");
 const dbJSON = require("../db/db.config.json");
 const axios = require("axios");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
+const { exec } = require("child_process");
+const { stdout, stderr } = require("process");
 
 const connectDB = () => {
   const dbName = dbJSON["cmms"];
@@ -14,93 +16,116 @@ const connectDB = () => {
   return client;
 };
 
+const createFeedbackRsync = async () => {
+  const { FEEDBACK_SERVER, FEEDBACK_SERVER_PORT, FEEDBACK_SERVER_HTTP } =
+    process.env;
+  const fileSrc = "./server/feedbackCSV";
+  const fileDes = `${FEEDBACK_SERVER}@${FEEDBACK_SERVER_HTTP}:${FEEDBACK_SERVER_PORT}:./server/feedbackCSV2`;
+  exec(`rsync -a ${fileSrc}/ ${fileDes}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (stderr) {
+      console.log(stderr);
+      return;
+    }
+    console.log(stdout);
+  });
+};
+
 const createFeedbacks = async () => {
-  const { FEEDBACK_SERVER, FEEDBACK_SERVER_PORT, FEEDBACK_SERVER_HTTP } = process.env;
+  const { FEEDBACK_SERVER, FEEDBACK_SERVER_PORT, FEEDBACK_SERVER_HTTP } =
+    process.env;
   const client = connectDB();
 
-  const directoryPath = './server/feedbackCSV2'; // Replace with the actual directory path
-  const yesterdayDate = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  const directoryPath = "./server/feedbackCSV2"; // Replace with the actual directory path
+  const yesterdayDate = moment().subtract(1, "days").format("YYYY-MM-DD");
 
   try {
-      const response = await axios.get(`${FEEDBACK_SERVER_HTTP}://${FEEDBACK_SERVER}:${FEEDBACK_SERVER_PORT}/api/feedback/csv/${yesterdayDate}`);
-      const fileArray = response.data;
+    const response = await axios.get(
+      `${FEEDBACK_SERVER_HTTP}://${FEEDBACK_SERVER}:${FEEDBACK_SERVER_PORT}/api/feedback/csv/${yesterdayDate}`
+    );
+    const fileArray = response.data;
 
-      const savedFiles = [];
+    const savedFiles = [];
 
-      for (const file of fileArray) {
-          const filename = file.filename;
-          const content = file.content;
+    for (const file of fileArray) {
+      const filename = file.filename;
+      const content = file.content;
 
-          const savePath = path.join(__dirname, '/../feedbackCSV2', filename);
-          const decodedContent = Buffer.from(content, 'utf8');
+      const savePath = path.join(__dirname, "/../feedbackCSV2", filename);
+      const decodedContent = Buffer.from(content, "utf8");
 
-          fs.writeFileSync(savePath, decodedContent);
-          
-          savedFiles.push(filename);
+      fs.writeFileSync(savePath, decodedContent);
+
+      savedFiles.push(filename);
+    }
+
+    console.log("Saved files:", savedFiles);
+
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.error("Error reading directory:", err);
+        return;
       }
 
-      console.log('Saved files:', savedFiles)
+      const filteredFiles = files.filter((file) =>
+        file.startsWith(yesterdayDate)
+      );
 
-      fs.readdir(directoryPath, (err, files) => {
-        if (err) {
-          console.error('Error reading directory:', err);
-          return;
-        }
+      filteredFiles.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
+        fs.readFile(filePath, "utf8", (err, data) => {
+          if (err) {
+            console.error("Error reading file:", err);
+            return;
+          }
+          // Process data from the file
+          let columnData = {};
+          const lines = data.split("\n");
 
-        const filteredFiles = files.filter(file => file.startsWith(yesterdayDate));
+          lines.forEach((line, lineIndex) => {
+            const columns = line.split(","); // Split line into columns based on comma (CSV)
 
-        filteredFiles.forEach(file => {
-          const filePath = path.join(directoryPath, file);
-          fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-              console.error('Error reading file:', err);
-              return;
-            }
-            // Process data from the file
-            let columnData = {};
-            const lines = data.split('\n');
+            if (lineIndex === 0) {
+              headers = columns.map((header) => header.trim());
+            } else {
+              columns.forEach((column, columnIndex) => {
+                const header = headers[columnIndex];
+                let value = column.trim();
+                if (!columnData[header]) {
+                  columnData[header] = "";
+                }
 
-            lines.forEach((line, lineIndex) => {
-              const columns = line.split(','); // Split line into columns based on comma (CSV)
+                value = value.replaceAll("^", ",");
 
-              if (lineIndex === 0) {
-                headers = columns.map(header => header.trim());
-              } else {
-                columns.forEach((column, columnIndex) => {
-                  const header = headers[columnIndex];
-                  let value = column.trim();
-                  if (!columnData[header]) {
-                    columnData[header] = "";
-                  }
+                columnData[header] = JSON.parse(value);
+              });
 
-                  value = value.replaceAll("^", ",");
-
-                  columnData[header] = JSON.parse(value);
-                });
-
-                axios.post(`${FEEDBACK_SERVER_HTTP}://${FEEDBACK_SERVER}:${FEEDBACK_SERVER_PORT}/api/feedback`, columnData).then((res) => {
+              axios
+                .post(
+                  `${FEEDBACK_SERVER_HTTP}://${FEEDBACK_SERVER}:${FEEDBACK_SERVER_PORT}/api/feedback`,
+                  columnData
+                )
+                .then((res) => {
                   console.log("Feedback created for " + filePath);
-                }).catch((err) => {
+                })
+                .catch((err) => {
                   //console.log(err.response);
                   console.log("Unable to create feedback");
                 });
-
-              }
-            });
-
+            }
           });
         });
-        
-        console.log('Files with date format YYYY-MM-DD:', filteredFiles);
       });
 
+      console.log("Files with date format YYYY-MM-DD:", filteredFiles);
+    });
   } catch (err) {
-      console.error('Error while fetching and saving files:', err);
+    console.error("Error while fetching and saving files:", err);
   }
-
-
-
-}
+};
 
 const main = async () => {
   try {
@@ -127,7 +152,7 @@ const start = async () => {
   module.exports = cron.schedule("0 0 7 * * *", () => {
     main();
   });
-}
+};
 
 module.exports = { start };
 
